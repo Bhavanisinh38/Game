894f71ac6cc6f31ab6bb8bc4666682a0a007f0f0
Bhavanisinh web <web@pmcommu.com>
2024-01-01 16:05:34 +0530

    Update Commonlayout.js

--- src/containers/PageAccount/Commonlayout.js
+++ src/containers/PageAccount/Commonlayout.js
@@ -44,13 +44,13 @@ export default function Account({ children }) {
                         Bank Detail
                     </NavLink>
 
-                    {/* <NavLink to="/notifications" className='amb-link'>
+                    <NavLink to="/notifications" className='amb-link'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                         </svg>
                         <span className="amb-num">1</span>
                         Notifications
-                    </NavLink> */}
+                    </NavLink>
 
                     <div className="amb-link">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M378 108a191.41 191.41 0 0170 148c0 106-86 192-192 192S64 362 64 256a192 192 0 0169-148M256 64v192" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
